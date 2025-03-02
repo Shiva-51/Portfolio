6731bd212db669054cfbd46a8671e6884023f795
Shiva <shivasharvesh0509@gmail.com>
2025-03-02 19:20:29 +0530

    final_fr

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,14 +9,18 @@ export const Projects = () => {
 
         <section className={styles.container} id="projects">
             <h2 className={styles.title}>Projects</h2>
-                <div className={styles.outerImgContainer}>
-                    <div className={styles.imgContainer}>
-                        <img src={sample}></img>
-                        <img src={sample}></img>
-                    </div>
+            
+            <div className={styles.containerForImg}>
+                <div className={styles.imgContainer}>
+                    <img src={sample} className={styles.image}/>
                     {/* <h2>MedGenie</h2> */}
-                    {/* <h2>MedGenie2</h2> */}
                 </div>
+                <div className={styles.imgContainer}>
+                    <img src={sample} className={styles.image}/>
+                    {/* <h2>MedGenie</h2> */}
+                </div>
+            </div>
+
         </section>
 
     );
